e9a06b166dc1fd69cea1bc345f297f1a7205d639
Sanderbratt <gustaf_sanderbratt@hotmail.com>
2023-06-18 13:46:35 +0200

    Added Sign up page functionality on login page

--- frontend/src/routes/Login.svelte
+++ frontend/src/routes/Login.svelte
@@ -1,4 +1,12 @@
+<script>
+    let showLogin = true
+</script>
+
+
+
 <div id="mainContainer">
+    {#if showLogin == true}
+    <h2>Login</h2>
     <form action="">
         <div id="usernameContainer">
             <label for="usernameInput">Username</label>
@@ -11,9 +19,31 @@
         </div>
         <div id="buttonContainer">
             <button type="submit">Login</button>
+            <button on:click={()=>(showLogin = false)}>Switch To Sign Up</button>
         </div>
     </form>
+
+    {:else}
+    <h2>Sign Up</h2>
+    <form action="">
+        <div id="usernameContainer">
+            <label for="usernameInput">Username</label>
+            <input name="usernameInput" type="text">
+        </div>
+    
+        <div id="passwordContainer">
+            <label for="passwordInput">Password</label>
+            <input name="passwordInput" type="text">
+        </div>
+        <div id="buttonContainer">
+            <button type="submit">Sign Up</button>
+            <button on:click={()=>(showLogin = true)}>Switch To Login</button>
+        </div>
+    </form>
+    {/if}
 </div>
+
+
 
 <style>
     #usernameContainer{
