6acf93dc2be374bf0f225bfe06c7d8dcd2dd5d6e
Andralodo <andrey.arronet@hotmail.se>
2023-07-05 14:47:08 +0200

    added frontend into docker and it works

--- applications/backend/src/app.js
+++ applications/backend/src/app.js
@@ -9,6 +9,16 @@ const app = express()
 const PORT = 8080
 app.use(bodyParser.json());
 
+app.use(function(request, response, next){
+
+	response.set("Access-Control-Allow-Origin", "*")
+	response.set("Access-Control-Allow-Methods", "*")
+	response.set("Access-Control-Allow-Headers", "*")
+	response.set("Access-Control-Expose-Headers", "*")
+
+	next()
+})
+
 app.use("/api/posts", postRoutes)
 app.use("/api/accounts", accountRoutes)
 app.use("/api/comments", commentRoutes)
